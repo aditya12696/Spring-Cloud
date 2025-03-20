053e341791bfe39c1e5040532676cef474c56523
aditya12696 <aditysharma127@gmail.com>
2025-03-20 17:15:32 +0530

    Aditya Add Spring Security Demo

--- src/main/java/com/springboot/HomeController.java
+++ src/main/java/com/springboot/HomeController.java
@@ -1,75 +1,34 @@
-package com.springboot;
+package com.anand.secureapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.springboot.dao.AlienDao;
-import com.springboot.model.Alien;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController
+public class HomeController 
 {
-	@Autowired
-	private AlienDao dao;
-	
-	@ModelAttribute
-	public void modelData(Model m) 
-	{
-	m.addAttribute("name","Aliens");
-	}
-	
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String home()
 	{
-		return "index";
+		return "home.jsp";
 	}
-	
-	@GetMapping("getAliens")
-	public String getAliens(Model m)
+	@RequestMapping("/login")
+	public String loginPage()
 	{
-		m.addAttribute("result", dao.getAliens());
-		return "showAliens";
-		
+		return "login.jsp";
 	}
-	
-	
-	@GetMapping("getAlien")
-	public String getAlien(@RequestParam int aid, Model m)
+	@RequestMapping("/logout-success")
+	public String logoutPage()
 	{
-		m.addAttribute("result", dao.getAlien(aid));
-		return "showAliens";
-		
+		return "logout.jsp";
 	}
-	
-	
-	@RequestMapping("add")
-	public String add(@RequestParam("num1")int i ,@RequestParam("num2") int j, Model m)
+	@RequestMapping("user")
+	@ResponseBody
+	public Principal user(Principal principal)
 	{
-		
-	
-		int num3 = i + j;
-		
-		m.addAttribute("num3",num3);
-		
-		return  "result";
-		
+		return principal;
 	}
 	
-	
-	@RequestMapping("addAlien")
-	public String addAlien(@ModelAttribute("result") Alien a)
-	{
-		dao.addAlien(a);
-		
-		return "showAliens";
-		
-	}
 }
